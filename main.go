27002bd557bd07cdc39cb3d3b2bcb7a9f8a61f0f
Gary Ho <garyho0916@gmail.com>
2023-04-20 13:53:20 -0700

    Implement scan folder method

--- main.go
+++ main.go
@@ -3,10 +3,57 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
+func scanFolderHelpler(path string, level int) []string {
+	path = strings.TrimSuffix(path, "/")
+
+	// get the file struct
+	f, err := os.Open(path)
+
+	// handle the err if any
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// File has a field name:
+	files, err := f.ReadDir(0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("found:")
+
+	for _, file := range files {
+		name := "dir"
+		if !file.IsDir() {
+			name = "file"
+		}
+		fmt.Printf("%10s: %s\n", name, file.Name())
+	}
+
+	return make([]string, 0)
+}
+
+func scanFolder(path string) []string {
+	return scanFolderHelpler(path, 0)
+}
+
 func scan(path string) {
-	fmt.Println("scan:", path)
+	// TODO: recursive scan for .git folders
+	gitFolders := scanFolder(path)
+
+	// TODO: Write path to the .visual-git file
+	// writeInSetting(gitFolders)
+
+	// Print the results
+	fmt.Println("Found: ")
+	for _, path := range gitFolders {
+		fmt.Println(path)
+	}
 }
 
 func stat(email string) {
